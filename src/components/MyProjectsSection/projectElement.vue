e6756d5832e7443aaa61391681e9e1e004213d1e
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-14 23:51:11 +0100

    add projectelement

---
+++ src/components/MyProjectsSection/projectElement.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="element-container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {},
+};
+</script>
+<style lang="scss" scoped>
+.element-container {
+  white-space: nowrap;
+  min-width: 400px;
+  min-height: 400px;
+  height: 60vh;
+
+  border-radius: 50px;
+
+  background-color: #1b1f24;
+}
+</style>
