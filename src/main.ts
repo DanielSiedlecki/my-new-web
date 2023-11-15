54b982964750872ae9171cad395f266677fe8722
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-15 23:35:21 +0100

    add fontawesome

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
 
 import './styles/GlobalStyles.scss'
 
+library.add(fab)
 createApp(App)
     .use(router)
     .mount('#app')
