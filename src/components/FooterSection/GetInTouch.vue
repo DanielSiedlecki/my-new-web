176d4f0fe30ab5a5d35d92a68ddc527731308ab1
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-19 11:12:16 +0100

    created GetInTouch to footer

---
+++ src/components/FooterSection/GetInTouch.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="title d-flex flex-row align-items-center gap-1">
+    <h1>Get In Touch</h1>
+    <i class="fas fa-arrow-right-long"></i>
+  </div>
+  <div class="row">
+    <div class="col-6 d-flex flex-column">
+      <span>Email ME:</span>
+      <span
+        >mrSiedlecki@outlook.com<i class="fas fa-arrow-right-long"></i
+      ></span>
+    </div>
+
+    <div class="col-6 d-flex flex-column">
+      <span>CALL ME:</span>
+      <span>+48 534 - 166 - 602<i class="fas fa-arrow-right-long"></i></span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
