ad68402f91e0b599b7ae22a2323296a12f6b2c2d
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-19 12:55:39 +0100

    implement information and get touch components

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,9 +1,23 @@
-<template></template>
+<template>
+  <div class="container w-100">
+    <div class="row">
+      <div class="col-6">
+        <my-personal-information></my-personal-information>
+      </div>
+      <div class="col-6">
+        <get-in-touch></get-in-touch>
+      </div>
+    </div>
+  </div>
+</template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import GetInTouch from "./FooterSection/GetInTouch.vue";
+import MyPersonalInformation from "./FooterSection/MyPersonalInformation.vue";
 
 export default defineComponent({
+  components: { MyPersonalInformation, GetInTouch },
   setup() {},
 });
 </script>
