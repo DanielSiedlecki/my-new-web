b8cc324ae51af58136068670ddbb039d052e4326
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-19 10:21:24 +0100

    create myPersonalInformation to footer section

---
+++ src/components/FooterSection/MyPersonalInformation.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="row d-flex flex-column">
+    <div class="row d-flex align-items-center">
+      <div class="col-4">
+        <img src="../../assets/Footer_elements/myPicture.png" alt="" class="" />
+      </div>
+      <div class="col-8">
+        <span><h1>Daniel Siedlecki</h1></span>
+        <span>Web Developer</span>
+      </div>
+    </div>
+    <div class="col-12">dol</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {},
+});
+</script>
+
+<style lang="scss" scoped>
+img {
+  width: 200px;
+  height: 200px;
+  border-radius: 200px;
+}
+</style>
