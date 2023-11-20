5d938715048464872c64239f54e10ec34a795e37
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-20 22:22:37 +0100

    styled footer components

--- src/components/FooterSection/GetInTouch.vue
+++ src/components/FooterSection/GetInTouch.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="title d-flex flex-row align-items-center gap-1">
-    <h1>Get In Touch <i class="fas fa-arrow-right-long"></i></h1>
-  </div>
-  <div class="row">
-    <div class="col-6 d-flex flex-column">
-      <span><h2>Email ME:</h2></span>
-      <span class="text_icon d-flex flex-row align-items-center"
-        ><p>mrSiedlecki@outlook.com</p>
-        <i class="fas fa-angle-right"></i
-      ></span>
+  <div class="container">
+    <div class="title d-flex flex-row align-items-center mb-3">
+      <h1>Get In Touch <i class="fas fa-arrow-right-long"></i></h1>
     </div>
+    <div class="row gap-2">
+      <div class="d-flex col-12 col-lg-6 flex-column gap-1" style="width: auto">
+        <span><h2>Email ME:</h2></span>
+        <span class="text_icon d-flex flex-row align-items-center gap-1"
+          ><p>mrSiedlecki@outlook.com</p>
+          <i class="fas fa-angle-right"></i
+        ></span>
+      </div>
 
-    <div class="col-6 d-flex flex-column">
-      <span><h2>CALL ME:</h2></span>
-      <span class="text_icon d-flex flex-row align-items-center"
-        ><p>+48 534 - 166 - 602</p>
-        <i class="fas fa-angle-right"></i
-      ></span>
+      <div class="d-flex col-12 col-lg-6 flex-column gap-1" style="width: auto">
+        <span><h2>CALL ME:</h2></span>
+        <span class="text_icon"
+          ><span class="line d-flex flex-row align-items-center gap-1"
+            ><p>+48 534 166 602</p>
+            <i class="fas fa-angle-right"></i></span
+        ></span>
+      </div>
     </div>
   </div>
 </template>
@@ -30,23 +33,34 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+@import "../../styles/GlobalStyles.scss";
+.title:hover {
+  cursor: pointer;
+  h1 {
+    i {
+      margin-left: 10px;
+      transition: 0.3s ease-in;
+    }
+  }
+}
 h1 {
-  font-size: 2.5vmax;
+  font-size: 2rem;
   color: white;
   i {
-    color: red;
+    color: $primary-color;
+    filter: brightness(120%);
   }
 }
 
 h2 {
   color: white;
-  font-size: 1.4rem;
+  font-size: 1rem;
   text-transform: uppercase;
   font-weight: 600;
 }
 .text_icon {
-  background: linear-gradient(red, red),
-    linear-gradient(to right, rgba(255, 0, 0, 1), rgba(255, 0, 180, 1));
+  background: linear-gradient(#8895aa, #8895aa),
+    linear-gradient(to right, white, white);
   background-size: 100% 0.3em, 0% 0.1em;
   background-position: 100% 100%, 0 100%;
   background-repeat: no-repeat;
@@ -54,7 +68,8 @@ h2 {
   p {
     font-size: 1.2rem;
     color: white;
-    margin: 0;
+    margin-bottom: 10px;
+    font-weight: 600;
   }
 
   i {
@@ -62,14 +77,16 @@ h2 {
     color: white;
     align-items: center;
     transition: 0.3s ease-in;
+    margin-bottom: 10px;
   }
 }
 
 .text_icon:hover {
-  background-size: 0 0em, 100% 0.1em;
+  background-size: 0 0em, 100% 0.3em;
+  cursor: pointer;
 
   i {
-    margin-left: 2%;
+    margin-left: 5px;
   }
 }
 </style>
--- src/components/FooterSection/MyPersonalInformation.vue
+++ src/components/FooterSection/MyPersonalInformation.vue
@@ -1,15 +1,38 @@
 <template>
-  <div class="row d-flex flex-column">
-    <div class="row d-flex align-items-center">
-      <div class="col-4">
-        <img src="../../assets/Footer_elements/myPicture.png" alt="" class="" />
+  <div class="container">
+    <div class="row d-flex flex-column gap-3">
+      <div class="row d-flex align-items-center">
+        <div class="col-4 col-lg-5 col-xl-3 text-center ma">
+          <img
+            src="../../assets/Footer_elements/myPicture.png"
+            alt=""
+            class="img-fluid"
+          />
+        </div>
+        <div class="col-8 col-lg-7 col-xl-9 gap-3">
+          <span class="title"><h1>Daniel Siedlecki</h1></span>
+          <span class="job_name"
+            ><h2 class="mt-2">Beginner Web Developer</h2></span
+          >
+        </div>
       </div>
-      <div class="col-8">
-        <span><h1>Daniel Siedlecki</h1></span>
-        <span>Web Developer</span>
+      <div class="col-12 mt-3">
+        <span class="icons d-flex gap-4 mb-2">
+          <i
+            v-for="(item, index) in iconsList"
+            :key="index"
+            :class="{
+              highlighted: index === hoveredIndex,
+              'not-highlighted': index !== hoveredIndex && hoveredEffect,
+              'not-highlighted-off': !hoveredEffect,
+              [item]: true,
+            }"
+            @mouseover="hoverElementEffect(index)"
+            @mouseleave="resetElementEffect()"
+          ></i>
+        </span>
       </div>
     </div>
-    <div class="col-12">dol</div>
   </div>
 </template>
 
@@ -17,14 +40,78 @@
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  setup() {},
+  data() {
+    return {
+      iconsList: ["fab fa-linkedin", "fab fa-facebook", "fab fa-github"],
+      hoveredIndex: null,
+      hoveredEffect: false,
+    };
+  },
+  methods: {
+    hoverElementEffect(index: any) {
+      this.hoveredIndex = index;
+      this.hoveredEffect = true;
+    },
+    resetElementEffect() {
+      this.hoveredIndex = null;
+      this.hoveredEffect = false;
+    },
+  },
 });
 </script>
 
 <style lang="scss" scoped>
+@import "../../styles/GlobalStyles.scss";
 img {
-  width: 200px;
-  height: 200px;
-  border-radius: 200px;
+  width: 125px;
+  height: 125px;
+  border-radius: 50%;
+  background-color: $second-background-color;
+  position: relative;
+}
+
+.title {
+  h1 {
+    font-size: 2rem;
+    color: white;
+    letter-spacing: 0.2px;
+  }
+}
+
+.job_name {
+  h2 {
+    font-size: 1.1rem;
+    color: #8895aa;
+    font-weight: 400;
+  }
+}
+
+.icons {
+  i {
+    font-size: 2rem;
+    color: white;
+  }
+}
+
+@media screen and (max-width: 576px) {
+  img {
+    width: 80px;
+    height: 80px;
+    border-radius: 80px;
+  }
+  .title {
+    h1 {
+      font-size: 1.5rem;
+      color: white;
+      letter-spacing: 0.2px;
+    }
+  }
+  .job_name {
+    h2 {
+      font-size: 1.2rem;
+      color: #8895aa;
+      font-weight: 400;
+    }
+  }
 }
 </style>
