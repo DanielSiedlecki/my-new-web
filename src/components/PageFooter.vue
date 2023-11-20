63f99ae0a62ecc1b6a42df08fa5a0b967fde1a6d
Daniel Siedlecki <siedleckidaniel2001@gmail.com>
2023-11-20 22:22:52 +0100

    corected footer responsive

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -1,12 +1,10 @@
 <template>
-  <div class="container w-100">
-    <div class="row">
-      <div class="col-6">
-        <my-personal-information></my-personal-information>
-      </div>
-      <div class="col-6">
-        <get-in-touch></get-in-touch>
-      </div>
+  <div class="line mt-5"></div>
+  <div class="container-fluid container-lg cont-border d-flex">
+    <div class="d-flex flex-column flex-lg-row w-100 gap-4 mt-5 mb-5">
+      <my-personal-information></my-personal-information>
+
+      <get-in-touch></get-in-touch>
     </div>
   </div>
 </template>
@@ -21,3 +19,14 @@ export default defineComponent({
   setup() {},
 });
 </script>
+
+<style lang="scss" scoped>
+.line {
+  width: 100%;
+  border-top: 1px solid #8895aa;
+}
+.cont-border {
+  margin-top: 5%;
+  margin-bottom: 5%;
+}
+</style>
